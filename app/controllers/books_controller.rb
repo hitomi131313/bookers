aed13be408f2bb9bab6c0172604f6119b548d648
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-07-15 13:38:35 +0000

    [Fix]flashmessage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully updated."
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
